c53e6294a88513aefa56a95441a01180b9542e73
PBEELEN <pieter-jan.beelen@capgemini.com>
2017-02-10 20:18:52 +0100

    working login

--- app-services/user.service.js
+++ app-services/user.service.js
@@ -13,33 +13,26 @@
         service.GetById = GetById;
         service.GetByUsername = GetByUsername;
         service.Create = Create;
-        service.Update = Update;
-        service.Delete = Delete;
 
+        var SERVICE_ENDPOINT = "http://192.168.101.223:3000";
         return service;
 
+
         function GetAll() {
-            return $http.get('/api/users').then(handleSuccess, handleError('Error getting all users'));
+            return $http.get(SERVICE_ENDPOINT + '/recruiters').then(handleSuccess, handleError('Error getting all users'));
         }
 
         function GetById(id) {
-            return $http.get('/api/users/' + id).then(handleSuccess, handleError('Error getting user by id'));
+            return $http.get(SERVICE_ENDPOINT + '/recruiters/' + id).then(handleSuccess, handleError('Error getting user by id'));
         }
 
         function GetByUsername(username) {
-            return $http.get('/api/users/' + username).then(handleSuccess, handleError('Error getting user by username'));
+            return $http.get(SERVICE_ENDPOINT + '/recruiters/' + username).then(handleSuccess, handleError('Error getting user by username'));
         }
 
         function Create(user) {
-            return $http.post('/api/users', user).then(handleSuccess, handleError('Error creating user'));
-        }
-
-        function Update(user) {
-            return $http.put('/api/users/' + user.id, user).then(handleSuccess, handleError('Error updating user'));
-        }
-
-        function Delete(id) {
-            return $http.delete('/api/users/' + id).then(handleSuccess, handleError('Error deleting user'));
+            console.log(JSON.stringify(user))
+            return $http.post(SERVICE_ENDPOINT + '/recruiters', JSON.stringify(user)).then(handleSuccess, handleError('Error creating user'));
         }
 
         // private functions
